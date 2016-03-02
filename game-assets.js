f03e8a238e3a4d70e94c1749b891f1f4b1276ef7
Csók Tamás <godhak@gmail.com>
2016-03-02 14:06:17 +0100

    Monthly progress

--- game-assets.js
+++ game-assets.js
@@ -4,3 +4,81 @@ if (Meteor.isServer) {
     };
 }
 Schema = {};
+
+Schema.CoreUser = new SimpleSchema({
+    username: {
+        type: String,
+        optional: false,
+        regEx: /^[^.?!@~_]+$/,
+        min: 3,
+        max: 20
+    }
+});
+
+Schema.CoreEmail = new SimpleSchema({
+    email: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email,
+        optional: false
+    }
+});
+
+Schema.UserId = new SimpleSchema({
+    userId: {
+        type: String,
+        optional: false
+    }
+});
+
+Schema.DoublePassword = new SimpleSchema({
+    password: {
+        type: String,
+        optional: false,
+        min: 6
+    },
+    passwordAgain: {
+        type: String,
+        optional: false,
+        min: 6
+    }
+});
+
+Schema.Progress = new SimpleSchema({
+    progress: {
+        type: Array,
+        optional: true
+    },
+    "progress.$": {
+        type: Object,
+        optional: true
+    },
+    "progress.$.startedAt": {
+        type: Date,
+        optional: true,
+        autoValue: function() {
+            if (this.isInsert || this.isUpdate) {
+                return new Date();
+            } else if (this.isUpsert) {
+                return {$setOnInsert: new Date()};
+            } else {
+                this.unset();  // Prevent user from supplying their own value
+            }
+        }
+    },
+    "progress.$.finishAt": {
+        type: Date,
+        optional: false
+    },
+    "progress.$.count": {
+        type: Number,
+        optional: false
+    }
+});
+
+Schema.i18n = new SimpleSchema({
+    i18n: {
+        type: Object,
+        blackbox: true,
+        optional: true
+    }
+});
--- package.js
+++ package.js
@@ -2,13 +2,14 @@ Package.describe({
   name: 'game-assets',
   version: '0.0.1',
   summary: 'Add localized initial content to Sigurvia',
-  git: 'http://vm.ik.bme.hu:19433/ucs/game-assets.git',
+  git: 'https://github.com/ucsgames/game-assets.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use("ejson");
+  api.use("aldeed:simple-schema@1.3.2");
   api.addFiles('game-assets.js');
   api.addAssets('i18n/buildings.json', 'server');
   api.export('GameContent', 'server');
